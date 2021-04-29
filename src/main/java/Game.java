6be66369516d2b14b8ebe4a5af6147b07eb02bb8
Droege <droege@saenger.io>
2021-04-29 20:13:50 +0200

    added player numberOfMines Input

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,15 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Game {
 
     public static void main(String[] args){
 
-        Field field = new Field(9, 9, 10);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many mines do you want to have on the field?");
+        int numbersOfMines = sc.nextInt();
+
+        Field field = new Field(9, 9, numbersOfMines);
         field.initField();
         field.showField();
         System.out.print(field.randNumbersList);
+
 
 
 
